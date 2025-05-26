b08310ab881f986ac4fa25dd6cce47d7de5bb0fa
u <kanprin06@gmail.com>
2025-05-26 20:56:05 +0300

    1

--- tours/urls.py
+++ tours/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', views.register, name='register'),
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('news/', views.news_list, name='news-list'),
